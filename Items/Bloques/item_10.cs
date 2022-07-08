c32d0955dc3a6d865ce168d8d00635ad307a3e84
RBTTIM <rbttimtreok@gmail.com>
2022-07-07 22:11:07 -0400

    Elementos de reliquias master

---
+++ Items/Bloques/item_10.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace opswordsII.Items.Bloques
+{
+    internal class item_10 : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.item_10_tile>(), 0);
+
+            Item.width = 30;
+            Item.height = 44;
+            Item.maxStack = 99;
+            Item.rare = ItemRarityID.Master;
+            Item.master = true;
+            Item.value = Item.buyPrice(0, 5);
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Reliquia de prueba N1");
+            Tooltip.SetDefault("Esta es una reliquia de prueba n1");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+    }
+}
