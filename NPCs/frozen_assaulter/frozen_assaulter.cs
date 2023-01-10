9c4db89192d34bfbaebd98c5c3c860b802e020c1
santiago garcia <sa.g.g.c.27@gmail.com>
2023-01-10 01:21:00 -0300

    Reescritura frozen

--- NPCs/frozen_assaulter/frozen_assaulter.cs
+++ NPCs/frozen_assaulter/frozen_assaulter.cs
@@ -20,15 +20,15 @@ using Terraria.Audio;
 using RemnantOfTheAncientsMod.VanillaChanges;
 using RemnantOfTheAncientsMod.World;
 using RemnantOfTheAncientsMod.Items.Bloques.Trophy;
+using System.IO;
 
 namespace RemnantOfTheAncientsMod.NPCs.frozen_assaulter
 {
     [AutoloadBossHead]
     public class frozen_assaulter : ModNPC
     {
-        int invincibilityTimer = 1;
+        int invincibilityTimer = 0;
         bool healAnimation = false;
-        bool fase3 = false;
         int currentPhase = 1;
         public override void SetStaticDefaults()
         {
@@ -59,147 +59,265 @@ namespace RemnantOfTheAncientsMod.NPCs.frozen_assaulter
             Music = MusicLoader.GetMusicSlot(Mod, "Sounds/Music/Frozen_Assaulter_p1");
             NPC.netAlways = true;
         }
+        private int attackCounter;
+
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(attackCounter);
+        }
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            attackCounter = reader.ReadInt32();
+        }
 
         public override void AI()
         {
-            Player player = Main.player[NPC.target];
-            float distance = NPC.Distance(Main.player[NPC.target].Center);
 
-            if (player.dead)
-            {
-                NPC.EncourageDespawn(7);
-                return;
-            }
-            if (NPC.target < 0 || NPC.target == 255 || Main.player[NPC.target].dead || !Main.player[NPC.target].active)
-            {
-                NPC.TargetClosest(true);
-            }
-            NPC.netUpdate = true;
-            NPC.ai[0]++;
 
-            NPC.ai[1]++;
-            phaseChanger();
-            if (!Reaper.ReaperMode)
+
+            //NPC.ai[0]++;
+
+            //NPC.ai[1]++;
+            //phaseChanger();
+            //if (!Reaper.ReaperMode)
+            //{
+            //    // if (NPC.ai[1] >= 5) shootIa((int)NpcChanges1.ExpertDamageScale(50, true), "Frozenp", player, 20f);
+            //    //if (NPC.ai[1] == 115)
+            //    //{
+            //    //    shootIa((int)NpcChanges1.ExpertDamageScale(30, true), "Lazer", player, 20f);
+            //    //    NPC.ai[1] = 0;
+            //    //}
+            //    NPC.ai[2]++;
+
+            //}
+            //else
+            //{
+            //  //  if (NPC.ai[1] >= 20) shootIa((int)NpcChanges1.ExpertDamageScale(180, true), "Frozenp", player, 40f);
+            // //   if (NPC.ai[1] == 115) shootIa((int)NpcChanges1.ExpertDamageScale(230, true), "Lazer", player, 30f);
+            //    NPC.ai[2]++;
+            //}
+
+            //if (NPC.ai[2] % 600 == 3)
+            //{  //Npc spown rate  // 230 is projectile fire rate
+            //   // NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.position.X, (int)NPC.position.Y, NPCID.IceElemental);
+            //}
+            //if (Main.expertMode)
+            //{
+            //   /* if (NPC.life < NPC.lifeMax / 2)
+            //    {
+            //        if (NPC.ai[2] % 900 == 1)
+            //        {
+            //            NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.position.X, (int)NPC.position.Y, NPCType<IGolem>());
+            //        }
+            //    }*/
+            //}
+            //if (NPC.ai[2] == 950)
+            //{
+            ////    NPC.ai[2] = 0;
+            //}
+
+            //NPC.ai[3]++;
+
+            //if (NPC.life < NPC.lifeMax / 4)
+            //{
+            //    phaseChanger();
+            //    fase3 = true;
+            //}
+            //if (Main.expertMode)
+            //{
+
+            //    if (fase3)
+            //    {
+
+            //        Music = MusicLoader.GetMusicSlot(Mod, "Sounds/Music/Frozen_Assaulter_p2");
+            //        NPC.dontTakeDamage = true;
+
+            //        if (NPC.ai[3] > 388) NPC.Center = Main.player[NPC.target].Center + new Vector2(Main.rand.Next(-250 * 2, 150 * 2), Main.rand.Next(-250 * 2, 150 * 2));
+            //        if (invincibilityTimer <= 1304) invincibilityTimer++;
+            //        if (invincibilityTimer >= 1300) NPC.dontTakeDamage = false;
+            //        if (invincibilityTimer >= 1303)
+            //        {
+            //            if (!healAnimation)
+            //            {
+            //                if (!Reaper.ReaperMode)
+            //                {
+            //                    for (int i = NPC.lifeMax / 4; i <= NPC.lifeMax / 2; i++)
+            //                    {
+            //                        NPC.life = i;
+            //                    }
+            //                }
+            //                else
+            //                {
+            //                    for (int i = NPC.lifeMax / 4; i <= NPC.lifeMax / 1.5; i++)
+            //                    {
+            //                        NPC.life = i;
+            //                    }
+            //                }
+            //                healAnimation = true;
+            //            }
+            //        }
+
+            //    }
+            //    //npc.Center = new Vector2(Main.rand.Next(-5 * 1, 190 * 2), Main.rand.Next(-100 * 2, 200 * 2));}  
+            //    if (NPC.ai[3] == 390)
+            //    {
+            //        NPC.ai[3] = 0;
+            //    }
+            //}
+
+
+
+
+            if (Main.netMode != NetmodeID.MultiplayerClient)
             {
-                if (NPC.ai[1] >= 5) shootIa((int)NpcChanges1.ExpertDamageScale(50, true), "Frozenp", player, 20f);
-                if (NPC.ai[1] == 115)
+                Player target = Main.player[NPC.target];
+                float distance = NPC.Distance(Main.player[NPC.target].Center);
+
+                if (target.dead)
                 {
-                    shootIa((int)NpcChanges1.ExpertDamageScale(30, true), "Lazer", player, 20f);
-                    NPC.ai[1] = 0;
+                    NPC.EncourageDespawn(7);
+                    return;
                 }
-                NPC.ai[2]++;
-
-            }
-            else
-            {
-                if (NPC.ai[1] >= 20) shootIa((int)NpcChanges1.ExpertDamageScale(180, true), "Frozenp", player, 40f);
-                if (NPC.ai[1] == 115) shootIa((int)NpcChanges1.ExpertDamageScale(230, true), "Lazer", player, 30f);
-                NPC.ai[2]++;
-            }
-
-            if (NPC.ai[2] % 600 == 3)
-            {  //Npc spown rate  // 230 is projectile fire rate
-                NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.position.X, (int)NPC.position.Y, NPCID.IceElemental);
-            }
-            if (Main.expertMode)
-            {
-                if (NPC.life < NPC.lifeMax / 2)
+                if (NPC.target < 0 || NPC.target == 255 || Main.player[NPC.target].dead || !Main.player[NPC.target].active)
                 {
-                    if (NPC.ai[2] % 900 == 1)
+                    NPC.TargetClosest(true);
+                }
+                if (attackCounter > 0)
+                {
+                    attackCounter--; // tick down the attack counter.
+                    phaseChanger();
+
+                }
+                if (!Reaper.ReaperMode)
+                {
+                    switch (attackCounter)
                     {
-                        NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.position.X, (int)NPC.position.Y, NPCType<IGolem>());
+                        case 115:
+                            shootIa((int)NpcChanges1.ExpertDamageScale(30, true), ProjectileID.FrostBeam, target, 20f,0.5,0.5);
+                            break;
+                        case 300:
+                            NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.position.X, (int)NPC.position.Y, NPCID.IceElemental);
+                            break;
+                        case 850:
+                            if (currentPhase >= 2 && (Main.expertMode || Main.masterMode))
+                            {
+                                NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.position.X, (int)NPC.position.Y, NPCType<IGolem>());
+                            }
+                            break;
+                        case > 1:
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 20f,0.5,0.5);
+                            break;
                     }
                 }
-            }
-            if (NPC.ai[2] == 950)
-            {
-                NPC.ai[2] = 0;
-            }
-
-            NPC.ai[3]++;
-
-            if (NPC.life < NPC.lifeMax / 4)
-            {
-                phaseChanger();
-                fase3 = true;
-            }
-            if (Main.expertMode)
-            {
-
-                if (fase3)
+                else
                 {
-
-                    Music = MusicLoader.GetMusicSlot(Mod, "Sounds/Music/Frozen_Assaulter_p2");
-                    NPC.dontTakeDamage = true;
-
-                    if (NPC.ai[3] > 388) NPC.Center = Main.player[NPC.target].Center + new Vector2(Main.rand.Next(-250 * 2, 150 * 2), Main.rand.Next(-250 * 2, 150 * 2));
-                    if (invincibilityTimer <= 1304) invincibilityTimer++;
-                    if (invincibilityTimer >= 1300) NPC.dontTakeDamage = false;
-                    if (invincibilityTimer >= 1303)
+                    switch (attackCounter)
                     {
-                        if (!healAnimation)
-                        {
-                            if (!Reaper.ReaperMode)
+                        case 115:
+                            shootIa((int)NpcChanges1.ExpertDamageScale(30, true), ProjectileID.FrostBeam, target, 30f,0.5,0.5);
+                            break;
+                        case 150:
+                            NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.position.X, (int)NPC.position.Y, NPCID.IceElemental);
+                            break;
+                        case 600:
+                            if (currentPhase >= 2 && (Main.expertMode || Main.masterMode))
                             {
-                                for (int i = NPC.lifeMax / 4; i <= NPC.lifeMax / 2; i++)
-                                {
-                                    NPC.life = i;
-                                }
+                                NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.position.X, (int)NPC.position.Y, NPCType<IGolem>());
                             }
-                            else
-                            {
-                                for (int i = NPC.lifeMax / 4; i <= NPC.lifeMax / 1.5; i++)
-                                {
-                                    NPC.life = i;
-                                }
-                            }
-                            healAnimation = true;
-                        }
+                            break;
+                        case >= 0:
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, 2, 2);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, -2, -2);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, 5, 5);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, -5, -5);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, 50, 50);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, -50, -50);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, 100, 100);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, -100, -100);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, 150, 150);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, -150, -150);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, 200, 200);
+                            shootIa((int)NpcChanges1.ExpertDamageScale(50, true), ProjectileType<Frozenp>(), target, 70f, -200, -200);
+                            break;
+                    }
+
+                }                
+
+                if (Main.expertMode)
+                {
+                    if(currentPhase == 3)
+                    {
+                        FrozenPhase3();
                     }
 
                 }
-                //npc.Center = new Vector2(Main.rand.Next(-5 * 1, 190 * 2), Main.rand.Next(-100 * 2, 200 * 2));}  
-                if (NPC.ai[3] == 390)
+
+                if (attackCounter <= 0 && Vector2.Distance(NPC.Center, target.Center) < 200 && Collision.CanHit(NPC.Center, 1, 1, target.Center, 1, 1))
                 {
-                    NPC.ai[3] = 0;
+                    attackCounter = 900;
+                    NPC.netUpdate = true;
                 }
             }
         }
-        public void shootIa(int dammage, string tipo, Player P, float speed)
+        public void shootIa(int dammage, int type, Player P, float Speed, double x, double y)
         {
-            if (tipo == "Frozenp")
+            Vector2 vector8 = new Vector2(NPC.position.X + (NPC.width / 2), NPC.position.Y + (NPC.height / 2));
+            float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * x)), vector8.X - (P.position.X + (P.width * y)));
+            Projectile.NewProjectile(NPC.GetSource_FromAI(), vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, 10, 0f, 0);
+        }
+        public void FrozenPhase3()
+        {
+            Music = MusicLoader.GetMusicSlot(Mod, "Sounds/Music/Frozen_Assaulter_p2");
+
+
+            switch (invincibilityTimer)
             {
-                float Speed = speed;  
-                int damage = dammage; 
-                Vector2 vector8 = new Vector2(NPC.position.X + (NPC.width / 2), NPC.position.Y + (NPC.height / 2));
-                int type = ModContent.ProjectileType<Frozenp>();  
-                float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * 0.5f)), vector8.X - (P.position.X + (P.width * 0.5f)));
-                int num54 = Projectile.NewProjectile(NPC.GetSource_FromAI(), vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, 10, 0f, 0);
-            }
-            else if (tipo == "Lazer")
-            {
-                float Speed = speed;  
-                Vector2 vector8 = new Vector2(NPC.position.X + (NPC.width / 2), NPC.position.Y + (NPC.height / 2));
-                int damage = dammage; 
-                int type = ProjectileID.FrostBeam; 
-                float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * 0.5f)), vector8.X - (P.position.X + (P.width * 0.5f)));
-                int num54 = Projectile.NewProjectile(NPC.GetSource_FromAI(), vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, 10, 0f, 0);
+                case 0:
+                    invincibilityTimer = 1304;
+                    NPC.dontTakeDamage = true;
+                    break;
+                case 4:
+                    NPC.dontTakeDamage = false;
+                    invincibilityTimer--;
+                    break;
+                case 3:
+                    if (!healAnimation)
+                    {
+                        if (Reaper.ReaperMode)
+                        {
+                            NPC.life = NPC.lifeMax / 2;
+                        }
+                        else
+                        {
+                            NPC.life = NPC.lifeMax / 3;
+                        }
+                        healAnimation = true;
+                        invincibilityTimer--;
+                    }
+                    break;
+                case > 1:
+                    invincibilityTimer--;
+                        break;
             }
         }
         public void phaseChanger()
         {
-            if (NPC.life > NPC.lifeMax / 2) currentPhase = 1;
-            else if (NPC.life < NPC.lifeMax / 2 && !fase3) currentPhase = 2;
+            if (NPC.life > NPC.lifeMax / 2)
+            {
+                currentPhase = 1;
+            }
+            else if (NPC.life < NPC.lifeMax / 2 && NPC.life > NPC.lifeMax / 4)
+            {
+                currentPhase = 2;
+            }
             else if (NPC.life < NPC.lifeMax / 4)
             {
                 if (!healAnimation)
                 {
                     currentPhase = 3;
-                    fase3 = true;
                 }
-                else currentPhase = 4; 
+                else currentPhase = 4;
             }
-
         }
         public override void BossLoot(ref string name, ref int potionType)
         {
