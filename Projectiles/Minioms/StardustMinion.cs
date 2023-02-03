61132a95de86b783fc87eaba8bf05ad6a8508424
santiago garcia <sa.g.g.c.27@gmail.com>
2023-02-03 20:54:13 -0300

    Stardust minion porteado

---
+++ Projectiles/Minioms/StardustMinion.cs
@@ -0,0 +1,64 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using RemnantOfTheAncientsMod;
+using RemnantOfTheAncientsMod.Buffs;
+
+namespace opswordsII.Projectiles.Minioms
+{
+    public class StardustMinion : ModProjectile
+    {
+        public override string Texture => "Terraria/Projectile_" + ProjectileID.StardustDragon1;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("StardustMinion");
+            Main.projFrames[Projectile.type] = 6;
+            ProjectileID.Sets.MinionTargettingFeature[Projectile.type] = true;
+            Main.projPet[Projectile.type] = true;
+            ProjectileID.Sets.MinionSacrificable[Projectile.type] = true;   
+        }
+        public sealed override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.StardustDragon1);
+            Projectile.width = 10;
+            Projectile.height = 20;
+            Projectile.tileCollide = false;
+            Projectile.friendly = true;
+            Projectile.minion = true;
+            Projectile.minionSlots = 1f;
+            Projectile.penetrate = -1;
+            AIType = ProjectileID.StardustDragon1;
+        }
+        public override bool? CanCutTiles()
+        {
+            return false;
+        }
+        public override bool MinionContactDamage()
+        {
+            return true;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+            Player1 modPlayer = player.GetModPlayer<Player1>();
+            if (player.dead || !player.active)
+            {
+                player.ClearBuff(BuffType<StardustMinionBuff>());
+            }
+            if (player.HasBuff(BuffType<StardustMinionBuff>()))
+            {
+                Projectile.timeLeft = 2;
+            }
+            if (modPlayer.StardustMinion)
+            {
+                Projectile.timeLeft = 2;
+            }
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.immune[Projectile.owner] = 2;
+        }
+    }
+}
