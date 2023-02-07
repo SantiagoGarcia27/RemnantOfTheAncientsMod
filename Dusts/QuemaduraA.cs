daef14126804cea32e82a7a494f8548f41da3594
santiago garcia <sa.g.g.c.27@gmail.com>
2023-02-07 18:26:56 -0300

    Arreglado tifon de arena

--- Dusts/QuemaduraA.cs
+++ Dusts/QuemaduraA.cs
@@ -12,24 +12,26 @@ namespace RemnantOfTheAncientsMod.Dusts
 			dust.scale *= 0.7f;
 		}
 
-		public override bool MidUpdate(Dust dust) {
-			if (!dust.noGravity) {
+		public override bool MidUpdate(Dust dust)
+		{
+			if (!dust.noGravity)
+			{
 				dust.velocity.Y += 0.05f;
 			}
-
-			if (dust.noLight) {
+			if (dust.noLight)
+			{
 				return false;
 			}
 
 			float strength = dust.scale * 1.4f;
-			if (strength > 1f) {
+			if (strength > 1f)
+			{
 				strength = 1f;
 			}
 			Lighting.AddLight(dust.position, 0.1f * strength, 0.2f * strength, 0.7f * strength);
 			return false;
 		}
 
-		public override Color? GetAlpha(Dust dust, Color lightColor) 
-			=> new Color(lightColor.R, lightColor.G, lightColor.B, 25);
+		public override Color? GetAlpha(Dust dust, Color lightColor) => new Color(lightColor.R, lightColor.G, lightColor.B, 25);
 	}
 }
--- Projectiles/BossProjectiles/DesertTyphoon.cs
+++ Projectiles/BossProjectiles/DesertTyphoon.cs
@@ -1,11 +1,9 @@
-using System;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using RemnantOfTheAncientsMod.Buffs.Debuff;
 using RemnantOfTheAncientsMod.Dusts;
+using System;
 using Terraria;
 using Terraria.Audio;
-using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -18,10 +16,10 @@ namespace RemnantOfTheAncientsMod.Projectiles.BossProjectile
         public override bool Friendly => false;
         public override bool Hostile => true;
         public override int PenetrateKill => 10;
+        public override void SetStaticDefaults() => Main.projFrames[Projectile.type] = 4;
 
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
-            Projectile.rotation += (float)Projectile.direction * 0.8f;
             Projectile.penetrate--;
             if (Projectile.penetrate <= PenetrateKill) Projectile.Kill();
             else
@@ -35,8 +33,8 @@ namespace RemnantOfTheAncientsMod.Projectiles.BossProjectile
         }
         public override void AI()
         {
-            Projectile.rotation += (float)Projectile.direction * 0.8f;
-
+            AnimateTexture();
+            GenerateParticle();
             if (Projectile.alpha > 70)
             {
                 Projectile.alpha -= 15;
@@ -53,6 +51,26 @@ namespace RemnantOfTheAncientsMod.Projectiles.BossProjectile
             float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
             if (magnitude > 106f) vector *= 26f / magnitude;
         }
+        public void GenerateParticle()
+        {
+            for (int i = 0; i < ModContent.GetInstance<RemnantOfTheAncientsMod>().ParticlleMetter(5); i++)
+            {
+                if (Projectile.alpha <= 100)
+                {
+                    int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<QuemaduraA>());
+                    Main.dust[dust].velocity /= 0.5f;
+                }
+            }
+        }
+        public void AnimateTexture()
+        {
+            if (++Projectile.frameCounter >= 4)
+            {
+                Projectile.frameCounter = 0;
+                if (++Projectile.frame >= Main.projFrames[Projectile.type])
+                    Projectile.frame = 0;
+            }
+        }
     }
 
     public class DesertTyphoonFriendly : DesertTyphoonModel
@@ -60,11 +78,11 @@ namespace RemnantOfTheAncientsMod.Projectiles.BossProjectile
         public override bool Friendly => true;
         public override bool Hostile => false;
         public override int PenetrateKill => 30;
-
+        public override void SetStaticDefaults() => Main.projFrames[Projectile.type] = 4;
         public override void AI()
         {
-            Projectile.rotation += (float)Projectile.direction * 0.8f;
-
+            AnimateTexture();
+            GenerateParticle();
             if (Projectile.alpha > 70)
             {
                 Projectile.alpha -= 15;
@@ -97,7 +115,26 @@ namespace RemnantOfTheAncientsMod.Projectiles.BossProjectile
                     Projectile.velocity = (10 * Projectile.velocity + move) / 11f;
                     AdjustMagnitude(ref Projectile.velocity);
                 }
-
+            }
+        }
+        public void AnimateTexture()
+        {
+            if (++Projectile.frameCounter >= 4)
+            {
+                Projectile.frameCounter = 0;
+                if (++Projectile.frame >= Main.projFrames[Projectile.type])
+                    Projectile.frame = 0;
+            }
+        }
+        public void GenerateParticle()
+        {
+            for (int i = 0; i < ModContent.GetInstance<RemnantOfTheAncientsMod>().ParticlleMetter(5); i++)
+            {
+                if (Projectile.alpha <= 100)
+                {
+                    int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<QuemaduraA>());
+                    Main.dust[dust].velocity /= 0.5f;
+                }
             }
         }
 
@@ -129,14 +166,10 @@ namespace RemnantOfTheAncientsMod.Projectiles.BossProjectile
         public abstract bool Hostile { get; }
         public abstract int PenetrateKill { get; }
 
-        public override void SetStaticDefaults()
-        {
-            Main.projFrames[Projectile.type] = 3;
-        }
         public override void SetDefaults()
         {
-            Projectile.CloneDefaults(ProjectileID.DemonScythe);
-            Projectile.aiStyle = ProjectileID.Typhoon;
+            //Projectile.CloneDefaults(ProjectileID.DemonScythe);
+            //Projectile.aiStyle = ProjectileID.Typhoon;
             Projectile.width = 36;
             Projectile.height = 36;
             Projectile.friendly = Friendly;
@@ -147,37 +180,26 @@ namespace RemnantOfTheAncientsMod.Projectiles.BossProjectile
             Projectile.timeLeft = 200;
             Projectile.light = 0.75f;
             Projectile.extraUpdates = 1;
-            Main.projFrames[Projectile.type] = 3;
             Projectile.ignoreWater = true;
 
         }
         public override void AI()
         {
-            Projectile.light = 0.9f;
-            RemnantOfTheAncientsMod r = ModContent.GetInstance<RemnantOfTheAncientsMod>();
-            int NUM_DUSTS = r.ParticlleMetter(5);
-            for (int i = 0; i < NUM_DUSTS; i++)
-            {
-                if (Projectile.alpha <= 100)
-                {
-                    int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<QuemaduraA>());
-                    Main.dust[dust].velocity /= 0.5f;
-                }
-            }
+            Projectile.light = 1.0f;
+
         }
-       
 
         public override bool PreDraw(ref Color lightColor)
         {
-            Main.instance.LoadProjectile(Projectile.type);
-            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
-            for (int k = 0; k < Projectile.oldPos.Length; k++)
-            {
-                Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
-                Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
-                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
-            }
+            /* Main.instance.LoadProjectile(Projectile.type);
+             Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+             Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+             for (int k = 0; k < Projectile.oldPos.Length; k++)
+             {
+                 Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                 Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+                 Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+             }*/
 
             return true;
         }
