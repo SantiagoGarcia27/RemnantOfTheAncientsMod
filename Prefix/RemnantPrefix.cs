32776a305be5ba3cf7235f39a3e5ac3fbbcb9cc2
santiago garcia <sa.g.g.c.27@gmail.com>
2023-03-24 01:49:49 -0300

    Añadido crafteo Terragun,añadidos prefijos Berserk y Relic

--- Prefix/RemnantPrefix.cs
+++ Prefix/RemnantPrefix.cs
@@ -12,12 +12,13 @@ namespace RemnantOfTheAncientsMod.Prefixe
 		// Change your category this way, defaults to PrefixCategory.Custom. Affects which items can get this prefix.
 		public override PrefixCategory Category => PrefixCategory.Ranged;
 
+
 		// See documentation for vanilla weights and more information.
 		// In case of multiple prefixes with similar functions this can be used with a switch/case to provide different chances for different prefixes
 		// Note: a weight of 0f might still be rolled. See CanRoll to exclude prefixes.
 		// Note: if you use PrefixCategory.Custom, actually use ModItem.ChoosePrefix instead.
 		public override float RollChance(Item item) {
-			return 5f;
+			return 0.1f;//5
 		}
 
 		// Determines if it can roll at all.
@@ -31,19 +32,113 @@ namespace RemnantOfTheAncientsMod.Prefixe
 		public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus) {
 			damageMult *= 1f + 0.20f * Power;
 			shootSpeedMult *= 1f + 0.30f;
-			useTimeMult *= 0.60f;//speed
+			useTimeMult *= 0.70f;//speed
 			critBonus += 10;
 			knockbackMult *= 1.20f;
 		}
 
 		// Modify the cost of items with this modifier with this function.
 		public override void ModifyValue(ref float valueMult) {
-			valueMult *= 1f + 0.15f * Power;
+			valueMult *= 1f + 0.10f * Power;
 		}
 
 		// This is used to modify most other stats of items which have this modifier.
 		public override void Apply(Item item) {
-			//
 		}
 	}
+    public class Berserk : ModPrefix
+    {
+        // We declare a custom *virtual* property here, so that another type, ExampleDerivedPrefix, could override it and change the effective power for itself.
+        public virtual float Power => 1f;
+
+        // Change your category this way, defaults to PrefixCategory.Custom. Affects which items can get this prefix.
+        public override PrefixCategory Category => PrefixCategory.Melee;
+
+
+        // See documentation for vanilla weights and more information.
+        // In case of multiple prefixes with similar functions this can be used with a switch/case to provide different chances for different prefixes
+        // Note: a weight of 0f might still be rolled. See CanRoll to exclude prefixes.
+        // Note: if you use PrefixCategory.Custom, actually use ModItem.ChoosePrefix instead.
+        public override float RollChance(Item item)
+        {
+            return 0.1f;//5
+        }
+
+        // Determines if it can roll at all.
+        // Use this to control if a prefix can be rolled or not.
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        // Use this function to modify these stats for items which have this prefix:
+        // Damage Multiplier, Knockback Multiplier, Use Time Multiplier, Scale Multiplier (Size), Shoot Speed Multiplier, Mana Multiplier (Mana cost), Crit Bonus.
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            damageMult *= 1f + 0.20f * Power;
+          //  shootSpeedMult *= 1f + 0.30f;
+            useTimeMult *= 0.70f;//speed
+            critBonus += 10;
+            knockbackMult *= 1.20f;
+            scaleMult *= 1.20f;
+        }
+
+        // Modify the cost of items with this modifier with this function.
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1f + 0.10f * Power;
+        }
+
+        // This is used to modify most other stats of items which have this modifier.
+        public override void Apply(Item item)
+        {
+        }
+    }
+    public class Relic : ModPrefix
+    {
+        // We declare a custom *virtual* property here, so that another type, ExampleDerivedPrefix, could override it and change the effective power for itself.
+        public virtual float Power => 1f;
+
+        // Change your category this way, defaults to PrefixCategory.Custom. Affects which items can get this prefix.
+        public override PrefixCategory Category => PrefixCategory.Magic;
+
+
+        // See documentation for vanilla weights and more information.
+        // In case of multiple prefixes with similar functions this can be used with a switch/case to provide different chances for different prefixes
+        // Note: a weight of 0f might still be rolled. See CanRoll to exclude prefixes.
+        // Note: if you use PrefixCategory.Custom, actually use ModItem.ChoosePrefix instead.
+        public override float RollChance(Item item)
+        {
+            return 0.1f;//5
+        }
+
+        // Determines if it can roll at all.
+        // Use this to control if a prefix can be rolled or not.
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        // Use this function to modify these stats for items which have this prefix:
+        // Damage Multiplier, Knockback Multiplier, Use Time Multiplier, Scale Multiplier (Size), Shoot Speed Multiplier, Mana Multiplier (Mana cost), Crit Bonus.
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            damageMult *= 1f + 0.20f * Power;
+            useTimeMult *= 0.70f;//speed
+            critBonus += 10;
+            knockbackMult *= 1.20f;
+            manaMult *= 0.6f;
+        }
+
+        // Modify the cost of items with this modifier with this function.
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1f + 0.10f * Power;
+        }
+
+        // This is used to modify most other stats of items which have this modifier.
+        public override void Apply(Item item)
+        {
+        }
+    }
 }
