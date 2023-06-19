9c1876110d20e5b2802cdb771215e0190deb5037
santiago garcia <sa.g.g.c.27@gmail.com>
2023-06-19 00:30:14 -0300

    arreglo de descripción

--- RemnantOfTheAncientsMod.cs
+++ RemnantOfTheAncientsMod.cs
@@ -1,8 +1,9 @@
-using Terraria;
+﻿using Terraria;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using RemnantOfTheAncientsMod.Content.Items.Items;
 using System;
+using RemnantOfTheAncientsMod.Common.UtilsTweaks;
 
 namespace RemnantOfTheAncientsMod
 {
@@ -15,20 +16,14 @@ namespace RemnantOfTheAncientsMod
         public static Mod Census;
         public static Mod RemnantOfTheAncients;
         public static bool DebuggMode;
-#pragma warning disable CS0649 // Field 'RemnantOfTheAncientsMod.CustomCurrencyId' is never assigned to, and will always have its default value 0
         public static int CustomCurrencyId;
-#pragma warning restore CS0649 // Field 'RemnantOfTheAncientsMod.CustomCurrencyId' is never assigned to, and will always have its default value 0
         public static int MaxRarity = GetMaxRarity();
         public static int MaxPlayers = 0;
         public RemnantOfTheAncientsMod()
         {
 
         }
-
-
-#pragma warning disable CS0672 // Member 'RemnantOfTheAncientsMod.AddRecipes()' overrides obsolete member 'Mod.AddRecipes()'. Add the Obsolete attribute to 'RemnantOfTheAncientsMod.AddRecipes()'.
         public override void AddRecipes()
-#pragma warning restore CS0672 // Member 'RemnantOfTheAncientsMod.AddRecipes()' overrides obsolete member 'Mod.AddRecipes()'. Add the Obsolete attribute to 'RemnantOfTheAncientsMod.AddRecipes()'.
         {
             RecipeMaker.AddRecipes();
         }
@@ -48,6 +43,7 @@ namespace RemnantOfTheAncientsMod
             {
                 ModContent.GetInstance<Terracoin>(); // Crea una instancia del tipo para registrar tu moneda personalizada.
             }
+            Utils1.ForceLoadModHJsonLocalization(this);
 
             //for (int i = 1; i < ItemLoader.ItemCount; i++)
             //{
